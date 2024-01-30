6c44178b718651f9c0c59df249838a7c151c557f
duph39199 <dutvph39199@fpt.edu.vn>
2024-01-30 13:02:22 +0700

    lan 5

--- assm/admin/js/list.js
+++ assm/admin/js/list.js
@@ -1,15 +1,21 @@
 // On DOM Load
 window.addEventListener("DOMContentLoaded", init);
+// R + D: Read + Delete
 
 function init() {
   renderProductList();
-  //call api
-  // getElem
-  // render UI
+}
+
+function handleDeleteProduct(id) {
+  // alert(id);
+  if (window.confirm("Ban co mua xoa san pham nay ko????")) {
+    // call api delete(id)
+    console.log("xoa", id);
+  }
 }
 
 function renderProductList() {
-  // b1: lay data api
+  // Lay Data API
   const productList = [
     {
       id: 1,
@@ -278,119 +284,58 @@ function renderProductList() {
       },
     },
   ];
-  // b2: create elm
-  const div = document.createElement("div");
+  // b2 tim cha de bam vao
+  const productListElm = document.getElementById("product-list");
 
-  div.classList.add("flex", "gap-2", "flex-wrap");
+  // b3: tao elment tbody
+  const tbody = document.createElement("tbody");
 
-  // b3: Update Content
-  div.innerHTML = `
-  ${productList
-    .map((product) => {
-      return `
-    <div
-    class="w-full max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700"
-  >
-    <a href="#">
-      <img
-        class="p-8 rounded-t-lg"
-        src="${product.image}"
-        alt="product image"
-      />
-    </a>
-    <div class="px-5 pb-5">
-      <a href="#">
-        <h5
-          class="text-xl font-semibold tracking-tight text-gray-900 dark:text-white"
+  // b4: Update content
+  tbody.innerHTML = `
+  <thead
+  class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400"
+>
+  <tr>
+    <th scope="col" class="px-6 py-3">Product name</th>
+    <th scope="col" class="px-6 py-3">Category</th>
+    <th scope="col" class="px-6 py-3">Price</th>
+    <th scope="col" class="px-6 py-3">Action</th>
+  </tr>
+</thead>
+<tbody>
+${productList
+  .map(
+    (product) => `
+        <tr
+        class="odd:bg-white odd:dark:bg-gray-900 even:bg-gray-50 even:dark:bg-gray-800 border-b dark:border-gray-700"
         >
-          ${product.title}
-        </h5>
-      </a>
-      <div class="flex items-center mt-2.5 mb-5">
-        <div class="flex items-center space-x-1 rtl:space-x-reverse">
-          <svg
-            class="w-4 h-4 text-yellow-300"
-            aria-hidden="true"
-            xmlns="http://www.w3.org/2000/svg"
-            fill="currentColor"
-            viewBox="0 0 22 20"
-          >
-            <path
-              d="M20.924 7.625a1.523 1.523 0 0 0-1.238-1.044l-5.051-.734-2.259-4.577a1.534 1.534 0 0 0-2.752 0L7.365 5.847l-5.051.734A1.535 1.535 0 0 0 1.463 9.2l3.656 3.563-.863 5.031a1.532 1.532 0 0 0 2.226 1.616L11 17.033l4.518 2.375a1.534 1.534 0 0 0 2.226-1.617l-.863-5.03L20.537 9.2a1.523 1.523 0 0 0 .387-1.575Z"
-            />
-          </svg>
-          <svg
-            class="w-4 h-4 text-yellow-300"
-            aria-hidden="true"
-            xmlns="http://www.w3.org/2000/svg"
-            fill="currentColor"
-            viewBox="0 0 22 20"
-          >
-            <path
-              d="M20.924 7.625a1.523 1.523 0 0 0-1.238-1.044l-5.051-.734-2.259-4.577a1.534 1.534 0 0 0-2.752 0L7.365 5.847l-5.051.734A1.535 1.535 0 0 0 1.463 9.2l3.656 3.563-.863 5.031a1.532 1.532 0 0 0 2.226 1.616L11 17.033l4.518 2.375a1.534 1.534 0 0 0 2.226-1.617l-.863-5.03L20.537 9.2a1.523 1.523 0 0 0 .387-1.575Z"
-            />
-          </svg>
-          <svg
-            class="w-4 h-4 text-yellow-300"
-            aria-hidden="true"
-            xmlns="http://www.w3.org/2000/svg"
-            fill="currentColor"
-            viewBox="0 0 22 20"
-          >
-            <path
-              d="M20.924 7.625a1.523 1.523 0 0 0-1.238-1.044l-5.051-.734-2.259-4.577a1.534 1.534 0 0 0-2.752 0L7.365 5.847l-5.051.734A1.535 1.535 0 0 0 1.463 9.2l3.656 3.563-.863 5.031a1.532 1.532 0 0 0 2.226 1.616L11 17.033l4.518 2.375a1.534 1.534 0 0 0 2.226-1.617l-.863-5.03L20.537 9.2a1.523 1.523 0 0 0 .387-1.575Z"
-            />
-          </svg>
-          <svg
-            class="w-4 h-4 text-yellow-300"
-            aria-hidden="true"
-            xmlns="http://www.w3.org/2000/svg"
-            fill="currentColor"
-            viewBox="0 0 22 20"
-          >
-            <path
-              d="M20.924 7.625a1.523 1.523 0 0 0-1.238-1.044l-5.051-.734-2.259-4.577a1.534 1.534 0 0 0-2.752 0L7.365 5.847l-5.051.734A1.535 1.535 0 0 0 1.463 9.2l3.656 3.563-.863 5.031a1.532 1.532 0 0 0 2.226 1.616L11 17.033l4.518 2.375a1.534 1.534 0 0 0 2.226-1.617l-.863-5.03L20.537 9.2a1.523 1.523 0 0 0 .387-1.575Z"
-            />
-          </svg>
-          <svg
-            class="w-4 h-4 text-gray-200 dark:text-gray-600"
-            aria-hidden="true"
-            xmlns="http://www.w3.org/2000/svg"
-            fill="currentColor"
-            viewBox="0 0 22 20"
-          >
-            <path
-              d="M20.924 7.625a1.523 1.523 0 0 0-1.238-1.044l-5.051-.734-2.259-4.577a1.534 1.534 0 0 0-2.752 0L7.365 5.847l-5.051.734A1.535 1.535 0 0 0 1.463 9.2l3.656 3.563-.863 5.031a1.532 1.532 0 0 0 2.226 1.616L11 17.033l4.518 2.375a1.534 1.534 0 0 0 2.226-1.617l-.863-5.03L20.537 9.2a1.523 1.523 0 0 0 .387-1.575Z"
-            />
-          </svg>
-        </div>
-        <span
-          class="bg-blue-100 text-blue-800 text-xs font-semibold px-2.5 py-0.5 rounded dark:bg-blue-200 dark:text-blue-800 ms-3"
-          >5.0</span
+        <th
+        scope="row"
+        class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white"
         >
-      </div>
-      <div class="flex items-center justify-between">
-        <span class="text-3xl font-bold text-gray-900 dark:text-white"
-          >$${product.price}</span
-        >
+        ${product.title.substring(0, 10)}...
+        </th>
+        <td class="px-6 py-4">${product.category}</td>
+        <td class="px-6 py-4">$${product.price}</td>
+        <td class="px-6 py-4 flex gap-2">
         <a
-          href="#"
-          class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
-          >Add to cart</a
+            href="./edit.html?productId=${product.id}"
+            class="font-medium text-blue-600 dark:text-blue-500 hover:underline"
+            >Edit</a
         >
-      </div>
-    </div>
-  </div>
-    `;
-    })
-    .join("")}
+        <button
+            onClick=handleDeleteProduct(${product.id})
+            class="font-medium text-red-600 dark:text-red-500 hover:underline"
+            >Remove</
+            button>
+        </td>
+    </tr>
+    `
+  )
+  .join("")}
 
- 
+</tbody>
   `;
 
-  // b4: Tim diem neo
-  const parentElm = document.getElementById("product-list");
-
-  // nhan lam con
-  parentElm.appendChild(div);
+  productListElm.appendChild(tbody);
 }
